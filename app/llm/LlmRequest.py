f6da5fde5d01a7f25eae6b3974b18b856f86126d
JahJajaka <r2e4d6@gmail.com>
2024-05-27 00:23:37 +0200

    Add input event handling with llm

---
+++ app/llm/LlmRequest.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field, root_validator
+from typing import Optional
+import os
+class LlmRequest(BaseModel):
+    prompt: Optional[str] = Field(None, exclude=True)
+    event: str
+
+    @root_validator(pre=True)
+    def construct_prompt(cls, values):
+        event = values.get("event")
+        print(f'lets construct prompt from the event: {event}')
+        if event:
+            try:
+                current_dir = os.getcwd()
+                print(f"Current directory: {current_dir}")
+
+                with open("app/llm/system_prompt.txt", "r") as file:
+                    system_prompt = file.read()
+                values['prompt'] = f"{system_prompt}{event}"
+            except FileNotFoundError:
+                raise ValueError("Text file not found")
+        return values
--- app/llm/llm.py
+++ app/llm/llm.py
@@ -1,4 +1,10 @@
 from langchain_cohere.llms import Cohere
+from langchain_openai import ChatOpenAI
+import os
 
 def get_model():
     return Cohere(max_tokens=256, temperature=0)
+
+def get_openai_model():
+    #gpt-3.5-turbo gpt-4o
+    return ChatOpenAI(model="gpt-3.5-turbo", api_key=os.getenv('OPENAI_KEY'))
